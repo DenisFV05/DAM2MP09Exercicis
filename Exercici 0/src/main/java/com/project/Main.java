c1c50f2f50be467ee507d41761dc2dfae60a9344
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-09-26 16:03:51 +0200

    .

--- Exercici 0/src/main/java/com/project/Main.java
+++ Exercici 0/src/main/java/com/project/Main.java
@@ -6,36 +6,32 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear un pool de 2 hilos
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
-        // Definir la primera tasca: registrar eventos del sistema
         Runnable registrarEventos = () -> {
             System.out.println(Thread.currentThread().getName() + " → Registrando eventos del sistema...");
             try {
-                Thread.sleep(2000); // Simular procesamiento
+                Thread.sleep(2000); // Simular
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             System.out.println(Thread.currentThread().getName() + " → Eventos registrados.");
         };
 
-        // Definir la segunda tasca: comprobar estado de la red
         Runnable comprobarRed = () -> {
             System.out.println(Thread.currentThread().getName() + " → Comprobando estado de la red...");
             try {
-                Thread.sleep(3000); // Simular procesamiento
+                Thread.sleep(3000); // Simular 
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             System.out.println(Thread.currentThread().getName() + " → Estado de la red comprobado.");
         };
 
-        // Enviar tasques al executor
+        // Enviar tasques 
         executor.execute(registrarEventos);
         executor.execute(comprobarRed);
 
-        // Cerrar executor (libera recursos)
         executor.shutdown();
     }
 }
