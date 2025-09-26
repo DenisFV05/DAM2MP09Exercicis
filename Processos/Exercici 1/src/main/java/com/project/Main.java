fca686d61ff8c44fbe13e1b1b4c44211968cc8e3
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-09-26 17:09:22 +0200

    continuar ejercicio 1

--- Processos/Exercici 1/src/main/java/com/project/Main.java
+++ Processos/Exercici 1/src/main/java/com/project/Main.java
@@ -1,37 +1,65 @@
 package com.project;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class Main {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ConcurrentHasmap <String, Integer> compteBanc = new ConcurrentHashMap<>();
+        
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        try { 
+            Runnable entraDades = () -> { // T1 INTRODUIR
+                compteBanc.put("saldo", 1000);
+                System.out.println("Thread 1: Dades inicials -> saldo: " + compteBanc.get("saldo"));
+            };
 
-        Runnable registrarEventos = () -> {
-            System.out.println(Thread.currentThread().getName() + " → Registrando eventos del sistema...");
-            try {
-                Thread.sleep(2000); // Simular
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println(Thread.currentThread().getName() + " → Eventos registrados.");
-        };
+            Runnable calculInteres = () -> { // T2 MODIFICAR
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100); // 1 segon per que la introducció termini
+                    int saldoActual = compteBanc.get("saldo");
+                    int saldoNou = saldoActual + saldoActual / 10; // +10%
+                    compteBanc.put("saldo", saldoNou);
+                    System.out.println("Thread 2: Dades amb interès -> saldo: " + compteBanc.get("saldo"));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            };
 
-        Runnable comprobarRed = () -> {
-            System.out.println(Thread.currentThread().getName() + " → Comprobando estado de la red...");
-            try {
-                Thread.sleep(3000); // Simular 
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println(Thread.currentThread().getName() + " → Estado de la red comprobado.");
-        };
+            
 
-        // Enviar tasques 
-        executor.execute(registrarEventos);
-        executor.execute(comprobarRed);
 
-        executor.shutdown();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
--- Processos/Exercici 2/src/main/java/com/project/Main.java
+++ Processos/Exercici 2/src/main/java/com/project/Main.java
@@ -6,32 +6,6 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-
-        Runnable registrarEventos = () -> {
-            System.out.println(Thread.currentThread().getName() + " → Registrando eventos del sistema...");
-            try {
-                Thread.sleep(2000); // Simular
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println(Thread.currentThread().getName() + " → Eventos registrados.");
-        };
-
-        Runnable comprobarRed = () -> {
-            System.out.println(Thread.currentThread().getName() + " → Comprobando estado de la red...");
-            try {
-                Thread.sleep(3000); // Simular 
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println(Thread.currentThread().getName() + " → Estado de la red comprobado.");
-        };
-
-        // Enviar tasques 
-        executor.execute(registrarEventos);
-        executor.execute(comprobarRed);
-
-        executor.shutdown();
+    
     }
 }
